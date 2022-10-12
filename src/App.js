55aa1ba3b9679a0c595b08a260a52b5dd0b5a19c
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-12 13:50:19 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>App component</h1>
+    <div>
+     <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <nav>
+            {/* <img src='images\logo.svg' alt='logo'/> */}
+            <h1>Fylo</h1>
+            <h3>Features</h3>
+            <h3>Team</h3>
+            <h3>Sign In</h3>
+        </nav>
+    );
+}
+export default Navbar;
