ee67b095acaeb3fc2bf2cff0713d024ee5736baf
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-12 14:53:37 +0300

    starting features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
+import Feature from './components/Feature';
 
 function App() {
   return (
     <div>
      <Navbar />
      <Intro />
+     <Feature />
+     <Feature />
+     <Feature />
+     <Feature />
     </div>
   );
 }
---
+++ src/components/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Feature.css';
+
+function Feature(){
+    return(
+        <div class='feature'>
+            Feature
+        </div>
+    );
+}
+export default Feature;
