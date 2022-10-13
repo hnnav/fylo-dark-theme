ebdba78461b1a030a25315999963e6c95eb9435a
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-13 09:59:52 +0300

    css fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ import '../styles/Intro.css';
 function Intro(){
     return(
         <div class='intro'>
-            <img src='images\illustration-intro.png' alt=''/>
+            <img src='images/illustration-intro.png' alt=''/>
             <h1>All your files in one secure location, accessible anywhere.</h1>
             <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
             <button>Get Started</button>
